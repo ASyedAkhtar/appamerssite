6204bc6ad72e72a487a9ba81f42351d8260145b7
Amer Akhtar <amersakhtar@gmail.com>
2024-05-11 18:34:18 -0400

    Added spring boot template

---
+++ server/src/test/java/site/amers/server/AppamerssiteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.amers.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppamerssiteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
